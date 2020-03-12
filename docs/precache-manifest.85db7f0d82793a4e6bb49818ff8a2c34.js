f3ee4b95e1e5bffc4b2392bcad1eefec3a655ed7
softAdd <soft.snake867@gmail.com>
2020-03-12 23:23:56 +0500

    add docs folder for deploy

---
+++ docs/precache-manifest.85db7f0d82793a4e6bb49818ff8a2c34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c5ef4536745a412d3795a780190d6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01e68c2d4711cb7a7894",
+    "url": "/static/css/main.41c83493.chunk.css"
+  },
+  {
+    "revision": "b1b072dd41e2326db98c",
+    "url": "/static/js/2.3cd7d789.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.3cd7d789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01e68c2d4711cb7a7894",
+    "url": "/static/js/main.0b9a49c8.chunk.js"
+  },
+  {
+    "revision": "05e587675f0de265d2c1",
+    "url": "/static/js/runtime-main.3c5051ad.js"
+  },
+  {
+    "revision": "d2e691bc4a2b696929172cb3d22ce8ba",
+    "url": "/static/media/Nunito-Regular.d2e691bc.ttf"
+  }
+]);
